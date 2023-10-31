a6b96649451bddb452b4857d3019659654dd44f9
Sandeep Chakraborty <105798887+sandeep-chakraborty@users.noreply.github.com>
2023-10-31 14:16:03 +0530

    adding files

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var leftDiceImage = document.querySelector(".img1");
+var RightDiceImage = document.querySelector(".img2");
+
+var diceImageFilename = `images/dice${randomNumber1}.png`;
+var diceImageFilename2 = `images/dice${randomNumber2}.png`;
+
+leftDiceImage.setAttribute("src", diceImageFilename);
+RightDiceImage.setAttribute("src", diceImageFilename2);
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").textContent = "🚩Player 1 wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").textContent = "🚩Player 2 wins";
+} else {
+  document.querySelector("h1").textContent = "Draw:(";
+}
+document.addEventListener("DOMContentLoaded", function () {
+  var reloadButton = document.getElementsByClassName("button")[0];
+  reloadButton.addEventListener("click", function () {
+    location.reload();
+  });
+});
